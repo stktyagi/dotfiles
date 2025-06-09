a4f6c09c688bd6854b0aaf900c06bdd9d2510470
stktyagi <stktyagi2810@gmail.com>
2025-06-09 15:28:24 +0530

    .config

--- nvim/lua/user/theme.lua
+++
@@ -1,9 +0,0 @@
-
-require('onedark').setup {
-  -- Set a style preset. 'dark' is default.
-  style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
-  transparent = true,
-  term_colors = true,
-}
-require('onedark').load()
-
--- waybar/scripts/tlp-status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the current power mode using tlp-stat
-power_mode=$(tlp-stat -s | grep "^Mode" | awk -F'= ' '{print $2}')
-
-# Output the power mode for Polybar
-echo "TLP: $power_mode"
-
